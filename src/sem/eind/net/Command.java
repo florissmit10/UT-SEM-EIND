8c2c53809a9f297f4ed07bd33cf4d6c4c582f923
Floris <florissmit10@gmail.com>
2013-01-29 12:20:53 +0100

    Menubegin gemaakt.

--- src/sem/eind/net/Command.java
+++ src/sem/eind/net/Command.java
@@ -1,18 +1,24 @@
 package sem.eind.net;
 
 public enum Command {
-	CHECKIN(0),
-	CHECKUIT(0),
-	RESERVERING(0),
-	ANNULEERRESERVERING(0),
-	REKENING(0);
+	CHECKIN(0, ""),
+	CHECKUIT(0, ""),
+	RESERVERING(0, ""),
+	ANNULEERRESERVERING(0, ""),
+	REKENING(0, "");
 		
 	private int arguments;
-	private Command(int args){
+	private String menuText;
+	private Command(int args, String text){
 		arguments=args;
+		menuText=text;
 	}
 	
 	public boolean enoughArguments(int arguments){
 		return this.arguments==arguments;
+	}
+	
+	public String toString(){
+		return menuText;
 	}
 }
